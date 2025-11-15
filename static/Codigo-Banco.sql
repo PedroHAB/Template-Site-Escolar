f561ebf1c42f6faa7abc8a51021a81f86a697b46
Pedro Henrick <pedro14henrick@gmail.com>
2025-11-15 15:18:40 -0300

    Adicionado o README

--- static/Codigo-Banco.sql
+++ static/Codigo-Banco.sql
@@ -1,14 +1,13 @@
-
+CREATE TABLE matriculas(
+	id serial PRIMARY KEY,
+	NUM VARCHAR(30) NOT NULL
+);
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
 	matricula INTEGER UNIQUE REFERENCES matriculas (id)
-);
-CREATE TABLE matriculas(
-	id serial PRIMARY KEY,
-	NUM VARCHAR(30) NOT NULL
 );
 CREATE TABLE noticias (
     id SERIAL PRIMARY KEY,
